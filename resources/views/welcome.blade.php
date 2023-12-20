c6a6956405637b717ce4ed6d8850db6c8716faea
amartinez <amartinez@elinkdesign.com>
2023-12-20 13:33:55 -0500

    Your commit message here

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,22 +21,44 @@
         <div class="w-full md:w-1/2">
 
             <div>
-                <h1 class="text-4xl font-bold mb-4">Header 1</h1>
-                <h2 class="text-2xl font-semibold mb-4">Header 2</h2>
-                <p class="mb-4">Your paragraph text here.</p>
+                <h1 class="text-4xl font-bold mb-4">Alejandro Martinez</h1>
+                <h2 class="text-2xl font-semibold mb-4">Full Stack Developer</h2>
+                <p class="mb-4">I create outstanding and user-friendly digital encounters.</p>
             </div>
             <nav class="mb-4">
                 <ul class="space-y-2">
-                    <li><a href="#" class="text-blue-500 hover:text-blue-600">Link 1</a></li>
-                    <li><a href="#" class="text-blue-500 hover:text-blue-600">Link 2</a></li>
+                    <li><a href="https://github.com/thedigialex" class="text-blue-500 hover:text-blue-600">Link 1</a></li>
+                    <li><a href="https://thedigialex.com" class="text-blue-500 hover:text-blue-600">Link 2</a></li>
+                    <li><a href="https://www.linkedin.com/in/alejandro-martinez-10b5b0a8/" class="text-blue-500 hover:text-blue-600">Link 2</a></li>
                     <!-- More list items -->
                 </ul>
             </nav>
             <nav class="flex">
-                <ul class="flex space-x-4">
-                    <li><a href="#" class="text-blue-500 hover:text-blue-600">Link A</a></li>
-                    <li><a href="#" class="text-blue-500 hover:text-blue-600">Link B</a></li>
-                    <!-- More list items -->
+
+                <ul class="ml-1 mt-8 flex items-center" aria-label="Social media">
+                    <li class="mr-5 text-xs">
+                        <a class="block hover:text-slate-200" href="https://github.com/thedigialex" target="_blank" rel="noreferrer noopener" aria-label="GitHub (opens in a new tab)">
+                            <span class="sr-only">GitHub</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-6 w-6" aria-hidden="true">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="mr-5 text-xs">
+                        <a class="block hover:text-slate-200" href="https://thedigialex.com" target="_blank" rel="noreferrer noopener" aria-label="My Website (opens in a new tab)">
+                            <span class="sr-only">Website</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true">
+                                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 16.057v-3.057h2.994c-.059 1.143-.212 2.24-.456 3.279-.823-.12-1.674-.188-2.538-.222zm1.957 2.162c-.499 1.33-1.159 2.497-1.957 3.456v-3.62c.666.028 1.319.081 1.957.164zm-1.957-7.219v-3.015c.868-.034 1.721-.103 2.548-.224.238 1.027.389 2.111.446 3.239h-2.994zm0-5.014v-3.661c.806.969 1.471 2.15 1.971 3.496-.642.084-1.3.137-1.971.165zm2.703-3.267c1.237.496 2.354 1.228 3.29 2.146-.642.234-1.311.442-2.019.607-.344-.992-.775-1.91-1.271-2.753zm-7.241 13.56c-.244-1.039-.398-2.136-.456-3.279h2.994v3.057c-.865.034-1.714.102-2.538.222zm2.538 1.776v3.62c-.798-.959-1.458-2.126-1.957-3.456.638-.083 1.291-.136 1.957-.164zm-2.994-7.055c.057-1.128.207-2.212.446-3.239.827.121 1.68.19 2.548.224v3.015h-2.994zm1.024-5.179c.5-1.346 1.165-2.527 1.97-3.496v3.661c-.671-.028-1.329-.081-1.97-.165zm-2.005-.35c-.708-.165-1.377-.373-2.018-.607.937-.918 2.053-1.65 3.29-2.146-.496.844-.927 1.762-1.272 2.753zm-.549 1.918c-.264 1.151-.434 2.36-.492 3.611h-3.933c.165-1.658.739-3.197 1.617-4.518.88.361 1.816.67 2.808.907zm.009 9.262c-.988.236-1.92.542-2.797.9-.89-1.328-1.471-2.879-1.637-4.551h3.934c.058 1.265.231 2.488.5 3.651zm.553 1.917c.342.976.768 1.881 1.257 2.712-1.223-.49-2.326-1.211-3.256-2.115.636-.229 1.299-.435 1.999-.597zm9.924 0c.7.163 1.362.367 1.999.597-.931.903-2.034 1.625-3.257 2.116.489-.832.915-1.737 1.258-2.713zm.553-1.917c.27-1.163.442-2.386.501-3.651h3.934c-.167 1.672-.748 3.223-1.638 4.551-.877-.358-1.81-.664-2.797-.9zm.501-5.651c-.058-1.251-.229-2.46-.492-3.611.992-.237 1.929-.546 2.809-.907.877 1.321 1.451 2.86 1.616 4.518h-3.933z" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="mr-5 text-xs">
+                        <a class="block hover:text-slate-200" href="https://www.linkedin.com/in/alejandro-martinez-10b5b0a8" target="_blank" rel="noreferrer noopener" aria-label="LinkedIn (opens in a new tab)">
+                            <span class="sr-only">LinkedIn</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true">
+                                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                            </svg>
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </div>
