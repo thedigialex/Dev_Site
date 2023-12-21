22e82d6441fe4051485aaa0ee9ba6fe6dae8dfd9
TheDigiAlex <alejandro11raym@gmail.com>
2023-12-20 21:14:30 -0500

    Major overhaul. Need to finish projects and styling,

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,26 +15,36 @@
     <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
+<body class="antialiased" style="background-image: linear-gradient(to bottom, #B4FEE7, #FEE7B4);">
+    <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+        <div class="lg:flex lg:justify-between lg:gap-4">
+            <header class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+                <div>
+                    <h1 class="text-4xl font-bold sm:text-5xl">Alejandro Martinez</h1>
+                    <h2 class="mt-3 text-lg sm:text-xl">Full Stack Developer</h2>
+                    <p class="mt-4 max-w-xs ">I figure shit out.</p>
+                    <nav class="nav hidden lg:block" aria-label="In-page jump links">
+                        <ul class="mt-16 w-max">
+                            <li>
+                                <a class="group flex items-center py-3" href="#about">
+                                    <span class="nav-text text-xs font-bold uppercase">About</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="group flex items-center py-3" href="#experience">
+                                    <span class="nav-text text-xs font-bold uppercase">Experience</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="group flex items-center py-3 active" href="#projects">
+                                    <span class="nav-text text-xs font-bold uppercase">Projects
 
-<body class="antialiased ">
-    <div class="flex justify-center py-24 px-24 mx-72">
-        <div class="w-full md:w-1/2">
-
-            <div>
-                <h1 class="text-4xl font-bold mb-4">Alejandro Martinez</h1>
-                <h2 class="text-2xl font-semibold mb-4">Full Stack Developer</h2>
-                <p class="mb-4">I create outstanding and user-friendly digital encounters.</p>
-            </div>
-            <nav class="mb-4">
-                <ul class="space-y-2">
-                    <li><a href="https://github.com/thedigialex" class="text-blue-500 hover:text-blue-600">Link 1</a></li>
-                    <li><a href="https://thedigialex.com" class="text-blue-500 hover:text-blue-600">Link 2</a></li>
-                    <li><a href="https://www.linkedin.com/in/alejandro-martinez-10b5b0a8/" class="text-blue-500 hover:text-blue-600">Link 2</a></li>
-                    <!-- More list items -->
-                </ul>
-            </nav>
-            <nav class="flex">
-
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
                 <ul class="ml-1 mt-8 flex items-center" aria-label="Social media">
                     <li class="mr-5 text-xs">
                         <a class="block hover:text-slate-200" href="https://github.com/thedigialex" target="_blank" rel="noreferrer noopener" aria-label="GitHub (opens in a new tab)">
@@ -60,14 +70,182 @@
                         </a>
                     </li>
                 </ul>
-            </nav>
-        </div>
-        <div class="w-full md:w-1/2">
-            <h1 class="text-4xl font-bold mb-4">Header 1</h1>
-            <h2 class="text-2xl font-semibold mb-4">Header 2</h2>
-            <p class="mb-4">Your paragraph text here.</p>
+            </header>
+            <main id="content" class="pt-24 lg:w-1/2 lg:py-24">
+                <section id="about" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me">
+                    <h2 class=" font-bold uppercase pb-4">About</h2>
+                    <div>
+                        <p class="mb-4">In 2014, I embarked on an academic journey at the University of Kentucky, majoring in Computer Science. My academic pursuits were further enriched by minoring in Computer Engineering and Mathematics, a decision influenced by the significant overlap in coursework. My profound passion for programming was ignited when a favorite game of mine was discontinued, inspiring me to reconstruct it from scratch. This project was a pivotal moment in my career, laying the foundation for my future endeavors.</p>
+                        <p class="mb-4">Currently, I am dedicated to enhancing and maintaining client websites in my role. My career trajectory began as a Support Developer, quickly advancing to a Managerial position, and eventually evolving into a Full Web Developer role. My commitment to professional growth is paralleled by my personal dedication to continuous learning. </p>
+                        <p class="mb-4">In my leisure time, I immerse myself in various projects, constantly seeking new opportunities to broaden my technical expertise and knowledge.</p>
+                    </div>
+                </section>
+                <section id="experience" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Work experience">
+                    <h2 class=" font-bold uppercase pb-4">Experience</h2>
+                    <div>
+                        <ol class="group/list">
+                            <li class="mb-12">
+                                <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                                    <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2022 — Present</header>
+                                    <div class="z-10 sm:col-span-6">
+                                        <h3 class="font-medium leading-snug">
+                                            <div>
+                                                <a href="https://elinkdesign.com/" target="_blank" rel="noreferrer noopener" aria-label="Web Developer at eLink Design (opens in a new tab)">
+                                                    <span>Web Developer · eLink Design
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </h3>
+                                        <p class="mt-2 text-sm leading-normal">In my role as a Mobile/Web Application Developer, I have been dedicated to delivering high-quality, robust production code for a diverse portfolio of projects. My work serves a wide range of esteemed clients, potentially including prestigious pharacmacy stores, healthcare companies, and impactful non-profits.</p>
+                                        <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">PHP</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Vue</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Symfony</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">React</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Bootstrap</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Doctrine</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">WordPress</div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mb-12">
+                                <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                                    <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2022 — Present</header>
+                                    <div class="z-10 sm:col-span-6">
+                                        <h3 class="font-medium leading-snug">
+                                            <div>
+                                                <a href="https://elinkdesign.com/" target="_blank" rel="noreferrer noopener" aria-label="Web Developer at eLink Design (opens in a new tab)">
+                                                    <span>Support Manager · eLink Design
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </h3>
+                                        <p class="mt-2 text-sm leading-normal">My responsibilities will extend beyond mere technical support, as I will take on a leadership role within the support department. This will involve closely collaborating with team members, sharing knowledge, and offering mentorship to foster a dynamic and productive work environment. Leveraging my technical expertise in areas such as CMS, SEO, and website maintenance, along with my familiarity with web development languages like HTML, CSS, and JavaScript, I will ensure the effective resolution of client issues and the continuous improvement of our support services.</p>
+                                        <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Responsive</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Maintenance</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Hosting</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Prioritization</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Empathy</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Mentorship</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Risk-Management</div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mb-12">
+                                <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                                    <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2018 to Present">2022 — October 2022</header>
+                                    <div class="z-10 sm:col-span-6">
+                                        <h3 class="font-medium leading-snug">
+                                            <div>
+                                                <a href="https://elinkdesign.com/" target="_blank" rel="noreferrer noopener" aria-label="Web Developer at eLink Design (opens in a new tab)">
+                                                    <span>Support Developer · eLink Design
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </h3>
+                                        <p class="mt-2 text-sm leading-normal">As a Support Developer, my responsibilities will encompass a blend of technical proficiency and collaborative problem-solving. I will actively engage in diagnosing and resolving complex technical issues, leveraging my expertise in web development languages such as HTML, CSS, JavaScript, and PHP. My role will be crucial in maintaining the functionality and performance of web applications, ensuring responsiveness and cross-browser compatibility.</p>
+                                        <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Debugging</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Communication</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Collaboration</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">API Integration</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Security</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">Adaptability</div>
+                                            </li>
+                                            <li class="mr-1.5 mt-2">
+                                                <div class="flex px-3 py-1 text-xs font-medium">User Experience</div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ol>
+                        <div class="mt-12">
+                            <a class="leading-tight font-semibold" aria-label="View Full Resume" href="https://thedigialex.com/wp-content/uploads/2023/12/Resume.pdf">
+                                <span>View Full Resume</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                <section id="projects" class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Selected projects">
+                    <h2 class=" font-bold uppercase pb-4">Projects</h2>
+                    <div>
+                        <ul class="group/list">
+                            <li class="mb-12">
+                                <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                                    <div class="z-10 sm:order-2 sm:col-span-6">
+                                        <h3>
+                                            <a class="inline-flex items-baseline font-medium leading-tight" href="https://google.com" target="_blank" aria-label="Project 1 (opens in a new tab)">
+                                                <span>Project · 1</span>
+                                            </a>
+                                        </h3>
+                                        <p class="mt-2 text-sm leading-normal">Project details</p>
+                                    </div>
+                                    <img src="{{ asset('images/dev_site_logo.png') }}" alt="Place holder" loading="lazy" width="200" height="48" decoding="async" data-nimg="1">
+                                </div>
+                            </li>
+
+                        </ul>
+                        <div class="mt-12">
+                            <a class="leading-tight font-semibold" aria-label="View Projects" href="https://thedigialex.com/projects/">
+                                <span>View Projects</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+                    <p>Laravel project dockerize running on a self hosted server styled using tailwind.</p>
+                </footer>
+            </main>
         </div>
     </div>
 </body>
-
 </html>
